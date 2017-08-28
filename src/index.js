9f4dcb94f687c08af0576c5dc998430325f316ae
Dbrandy <devdbrandy@gmail.com>
2017-08-28 20:15:58 +0100

    Build and load css|image|fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import logo from './logo.svg';
 
 function component() {
   const element = document.createElement('div');
 
   // imported lodash
   element.innerHTML = _.join(['Hello,', 'Webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div
+  const myLogo = new Image();
+  myLogo.src = logo;
+
+  element.appendChild(myLogo);
 
   return element;
 }
