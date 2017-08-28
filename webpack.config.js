f2208c37cbb484f6b708f2bd5abcbf33a35fb253
Dbrandy <devdbrandy@gmail.com>
2017-08-28 21:59:03 +0100

    [development] - Implement hot-module on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
-  devServer: { contentBase: './dist' },
+  devServer: { contentBase: './dist', hot: true },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({ title: 'Output Management' })
+    new HtmlWebpackPlugin({ title: 'Output Management' }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve('dist'),
-    publicPath: '/'
+    path: path.resolve('dist')
   }
 };
